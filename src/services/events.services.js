16a0362dea92eec3a0fb2bb4dea4f5a86d47a22d
jgodwin10 <easypeasy725@gmail.com>
2025-02-07 10:49:22 +0100

    deleted booking and waiting list model as it won't be needed due to concurrency/ race conditions handling... about to start writing test

--- src/services/events.services.js
+++ src/services/events.services.js
@@ -1,13 +1,14 @@
 import Event from "../models/event.model.js";
+import AppError from "../utils/Error.js";
 
 export default class EventService {
 	static async initializeEvent({ name, totalTickets }) {
-		if (!name || !totalTickets) throw new Error("All fields needs to be filled");
+		if (!name || !totalTickets) throw new AppError("All fields needs to be filled", 400);
 		return Event.createEvent({ name, totalTickets });
 	}
 
 	static async getEventStatus(eventId) {
-		if (!eventId) throw new Error("The event ID can't be empty");
+		if (!eventId) throw new AppError("The event ID can't be empty", 400);
 		return Event.getEventById(eventId);
 	}
 }
